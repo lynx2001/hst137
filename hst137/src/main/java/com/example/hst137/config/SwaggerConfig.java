9a102e019ec8c0a4edafe91de6d526ac4bad23f5
lynx2001 <kangaroo125@naver.com>
2025-01-23 01:30:22 +0900

    api-connect

---
+++ hst137/src/main/java/com/example/hst137/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.hst137.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .openapi("3.0.0")
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("HST137 Swagger API")
+                .description("This is the swagger document for CodeArena Swagger")
+                .version("1.0.0");
+    }
+}
--- hst137/src/main/java/com/example/hst137/controller/exercise/ExerciseController.java
+++ hst137/src/main/java/com/example/hst137/controller/exercise/ExerciseController.java
@@ -4,10 +4,14 @@ import com.example.hst137.dto.exercise.request.ExerciseCreateRequest;
 import com.example.hst137.dto.exercise.request.ExerciseUpdateRequest;
 import com.example.hst137.dto.exercise.response.ExerciseResponse;
 import com.example.hst137.service.exercise.ExerciseService;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ExerciseController {
@@ -16,27 +20,49 @@ public class ExerciseController {
     public ExerciseController(ExerciseService exerciseService) {
         this.exerciseService = exerciseService;
     }
-
+/*
+    @Operation(summary = "Save new exercise")
+    @ApiResponse(responseCode = "200OK", description = "Exercise saved successfully")
     @PostMapping("/exercise")
-    public void saveExercise(@RequestBody ExerciseCreateRequest request) {
+    public ResponseEntity<Map<String, String>> saveExercise(@RequestBody ExerciseCreateRequest request) {
         exerciseService.saveExercise(request);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Exercise saved successfully");
+        return ResponseEntity.ok(response);
+    }
+*/
+    @Operation(summary = "Save new exercise")
+    @ApiResponse(responseCode = "200OK", description = "Exercise saved successfully")
+    @PostMapping("/exercise")
+    public ResponseEntity<Void> saveExercise(@RequestBody ExerciseCreateRequest request) {
+        exerciseService.saveExercise(request);
+        return ResponseEntity.ok().build(); // 200 OK 응답만 반환
     }
 
+    @Operation(summary = "Get All Exercises", description = "Return list of all saved exercised.")
+    @ApiResponse(responseCode = "200OK", description = "List of exercises retrieved successfully")
     @GetMapping("/exercise")
     public List<ExerciseResponse> getExercises() {
         return exerciseService.getExercises();
     }
 
+    @Operation(summary = "Get exercise by ID", description = "Returns the details of a specific exercise by its ID.")
+    @ApiResponse(responseCode = "200OK", description = "Exercise details retrieved successfully")
     @GetMapping("/exercise/{id}")
     public ExerciseResponse getExercise(@PathVariable Long id) {
         return exerciseService.getExerciseById(id);
     }
 
+    @Operation(summary = "Update an exercise", description = "Updates an existing exercise with new details.")
+    @ApiResponse(responseCode = "200OK", description = "Exercise updated successfully")
     @PutMapping("/exercise")
     public void updateExercise(@RequestBody ExerciseUpdateRequest request) {
         exerciseService.updateExercise(request);
     }
 
+    @Operation(summary = "Delete an exercise", description = "Deletes an exercise by its ID.")
+    @ApiResponse(responseCode = "200OK", description = "Exercise deleted successfully")
     @DeleteMapping("/exercise")
     public void deleteExercise(@RequestParam Long id) {
         exerciseService.deleteExercise(id);
